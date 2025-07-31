9e640c9c4256f9d07aada27614bca77a736df8e2
ak204479@gmail.com <ak204479@gmail.com>
2025-08-01 02:30:41 +0530

    add message class

--- employee-service/src/main/java/com/es/controller/MessageController.java
+++ employee-service/src/main/java/com/es/controller/MessageController.java
@@ -1,24 +1,23 @@
-package com.ds.controller;
+package com.es.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
 @RestController
-//@RequestMapping("actuator")
 public class MessageController {
 	
 	@Value("${spring.boot.message}")
 	private String message;
 	
-	@GetMapping("message")
+	@GetMapping("user/message")
 	public String message() {
 		return message;
 	}
+	
 	@PostMapping("actuator/refresh")
 	public void refreshActuator() {}
 }
