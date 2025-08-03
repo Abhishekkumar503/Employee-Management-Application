01041a3a01864b564035f7952d485fa64db9bfa6
ak204479@gmail.com <ak204479@gmail.com>
2025-08-03 17:52:43 +0530

    Add Circuit breaker with Fallback method

--- employee-service/src/main/java/com/es/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/es/service/impl/EmployeeServiceImpl.java
@@ -18,6 +18,8 @@ import com.es.feign.APIClient;
 import com.es.repo.EmployeeRepo;
 import com.es.service.EmployeeService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 	
@@ -55,6 +57,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 	}
 
 	@Override
+	@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartmentByRestTemplate")
 	public ApiResponseDto getEmployeDetails(Long id) {
 		// TODO Auto-generated method stub
 		Employee emp = employeeRepo.findById(id).orElseThrow(()-> new RuntimeException("Id not found"));
@@ -73,6 +76,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 		return apiResponseDto;
 	}
 	
+	@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartmentByWebClient")
 	@Override
 	public ApiResponseDto getEmployeDetailsWebClient(Long id) {
 		// TODO Auto-generated method stub
@@ -95,6 +99,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 		return apiResponseDto;
 	}
 	
+	@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartmentByFeignClient")
 	@Override
 	public ApiResponseDto getEmployeDetailsFeignClient(Long id) {
 		// TODO Auto-generated method stub
@@ -113,6 +118,71 @@ public class EmployeeServiceImpl implements EmployeeService {
 		return apiResponseDto;
 	}
 	
+	// Calling Default Department by  RestTemplate
+	@CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+	public ApiResponseDto getDefaultDepartmentByRestTemplate(Long id , Exception exception) {
+		// TODO Auto-generated method stub
+		Employee emp = employeeRepo.findById(id).orElseThrow(()-> new RuntimeException("Id not found"));
+		
+		DepartmentDTO departmentDTO = new DepartmentDTO();
+		departmentDTO.setId(404);
+		departmentDTO.setDepartmentName("RestTemplate Default Department");
+		departmentDTO.setDepartmentCode("RDD");
+		departmentDTO.setDepartmentDescription("RestTemplate Default Department ( when Department service not working )");
+		
+		
+		System.out.println("In RestTemplate!!" + departmentDTO.toString());
+		
+//		Providing values into API respose 
+		ApiResponseDto apiResponseDto = new ApiResponseDto();
+		apiResponseDto.setEmployeeDTO(modelMapper.map(emp,EmployeeDTO.class));
+		apiResponseDto.setDepartmentDTO(departmentDTO);
+		
+		return apiResponseDto;
+	}
+	
+	
+	public ApiResponseDto getDefaultDepartmentByWebClient(Long id , Exception exception) {
+		// TODO Auto-generated method stub
+		Employee emp = employeeRepo.findById(id).orElseThrow(()-> new RuntimeException("Id not found"));
+
+		DepartmentDTO departmentDTO = new DepartmentDTO();
+		departmentDTO.setId(404);
+		departmentDTO.setDepartmentName("WebClient Default Department");
+		departmentDTO.setDepartmentCode("WDD");
+		departmentDTO.setDepartmentDescription("WebClient Default Department ( when Department service not working )");
+		
+		
+		System.out.println("In WebClient!!" + departmentDTO.toString());
+		
+//		Providing values into API respose 
+		ApiResponseDto apiResponseDto = new ApiResponseDto();
+		apiResponseDto.setEmployeeDTO(modelMapper.map(emp,EmployeeDTO.class));
+		apiResponseDto.setDepartmentDTO(departmentDTO);
+		
+		return apiResponseDto;
+	}
+	
+	
+	public ApiResponseDto getDefaultDepartmentByFeignClient(Long id , Exception exception) {
+		// TODO Auto-generated method stub
+		Employee emp = employeeRepo.findById(id).orElseThrow(()-> new RuntimeException("Id not found"));
+
+		DepartmentDTO departmentDTO = new DepartmentDTO();
+		departmentDTO.setId(404);
+		departmentDTO.setDepartmentName("Feign Default Department");
+		departmentDTO.setDepartmentCode("FDD");
+		departmentDTO.setDepartmentDescription("feign Default Department ( when Department service not working )");
+		
+		System.out.println("In FeignClient!!" + departmentDTO.toString());
+		
+//		Providing values into API respose 
+		ApiResponseDto apiResponseDto = new ApiResponseDto();
+		apiResponseDto.setEmployeeDTO(modelMapper.map(emp,EmployeeDTO.class));
+		apiResponseDto.setDepartmentDTO(departmentDTO);
+		
+		return apiResponseDto;
+	}
 	
 
 }
